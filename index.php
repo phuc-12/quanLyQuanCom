38c1d629b30668f5f60e0b7b00bc5e0f25bfc0db
phuc-12 <phucqua147@gmail.com>
2024-10-29 22:56:01 +0700

    test

--- index.php
+++ index.php
@@ -3,13 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Trang chủ</title>
 </head>
 <body>
-    <header class="header">
-            <div>1900 6750</div>
-            <div>Đăng ký | Đăng nhập</div>
-        </header>
+<div class="top-bar">
+        <div class="contact-info">
+            <span>📞 1900-1900</span>
+        </div>
+        <div class="opening-hours">
+            <span>🕒 Thứ 2 - Chủ nhật 7:00 - 18:00</span>
+        </div>
+        <div>
+            <a href="#">Đăng ký</a> | <a href="#">Đăng nhập</a>
+        </div>
+        <div class="search-icon"><button></button></div>
+    </div>
+
+    <nav class="main-nav">
+        <a href="#" class="home-button">Trang chủ</a>
+        <a href="#">Thực đơn</a>
+        <div class="logo">Logo</div>
+        <a href="#">Giới Thiệu</a>
+        <a href="#">Liên Hệ</a>
+    </nav>
+
+    <nav class="sub-nav">
+        <a href="#">Món chạy</a>
+        <a href="#">Món mặn</a>
+        <a href="#">Đồ uống</a>
+        <a href="#">Tráng Miệng</a>
+    </nav>
+
+    <div class="slider">
+        <p>Hình ảnh chạy</p>
+    </div>
 </body>
 </html>
