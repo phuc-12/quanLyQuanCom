9e663fd8cde3ffb577cbbfbb380d633e1cbf0045
luongcongtrieu <luongcongtrieu210603@gmail.com>
2024-10-31 18:18:43 +0700

    31/10admin

---
+++ view/admin/admin.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản Lý</title>
+    <link rel="stylesheet" type="text/css" href="../../css/admin.css">    
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container-fluid p-0">
+        <div id="ql_header">
+            <div class="logo">
+                <p>logo</p>
+            </div>
+
+            <div class="trangChu">
+                <h4>Trang Chủ</h4>
+            </div>
+        </div>
+
+        <div id="content">
+            <div class="nav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-tachometer"></i>
+                                Tổng quát
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-spoon"></i>
+                                Quản lý thực đơn
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-address-card"></i>
+                            Quản lý nhân viên
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-address-card-o"></i>
+                            Quản lý khách hàng
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-gift"></i>
+                            Quản lý chương trình khuyến mãi
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-archive"></i>
+                            Quản lý đơn hàng
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="section">
+                <h3 style="margin-bottom: 20px;"><b>TỔNG QUÁT</b></h3>
+
+                <div class="mger_option">
+                    <a class="option mger_food" href="management_food.php"></a>
+                    <a class="option mger_employ" href="#"></a>
+                    <a class="option mger_customer" href="#"></a>
+                    <a class="option mger_gift" href="#"></a>
+                    <a class="option mger_order" href="#"></a>
+                </div>
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
