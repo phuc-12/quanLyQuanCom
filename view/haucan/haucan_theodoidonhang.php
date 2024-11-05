15bae47c3dcede5594638c4a4aba32fe6aef4a4b
ngane140 <nthinga2003@gmail.com>
2024-11-05 14:52:01 +0700

    hau cần headerheader

---
+++ view/haucan/haucan_theodoidonhang.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản Lý Khách Hàng</title>
+    <link rel="stylesheet" type="text/css" href="../../css/haucan_css/style.css">
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+        <div class="container-fluid p-0">
+            <div id="ql_header">
+                <div class="logo">
+                    <p>logo</p>
+                </div>
+
+                <a class="trangChu" href="../../index.php">
+                    <h4>Trang Chủ</h4>
+                </a>
+            </div>
+        </div>
+    </header>
+</body>
