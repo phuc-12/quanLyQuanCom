761be33d0132229ac08078c4934502cc7164a364
ngane140 <nthinga2003@gmail.com>
2024-11-06 00:20:54 +0700

    Add files via upload

---
+++ view/haucan/haucan_theodoidonhang.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Theo dõi đơn hàng</title>
+    <link rel="stylesheet" type="text/css" href="../../css/haucan_css/style.css">
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+        <div class="container-fluid p-0">
+            <div id="ql_header">
+                <div class="logo">
+                    <p>logo</p>
+                </div>
+
+                <a class="trangChu" href="../../index.php">
+                    <h4>Trang Chủ</h4>
+                </a>
+            </div>
+        </div>
+    </header>
+    <div id="content">
+            <div class="container">
+                <div class="header-row">
+                    <h2>DANH SÁCH ĐƠN HÀNG</h2>
+                    <div class="search-add">
+                        <input type="text" placeholder="Tìm kiếm...">
+                        <button class="search-button">🔍</button>
+                    </div>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>Mã đơn hàng</th>
+                            <th>Tên khách hàng</th>
+                            <th>Địa chỉ giao hàng</th>
+                            <th>Thời gian yêu cầu giao</th>
+                            <th>Hành động</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>DH01</td>
+                            <td>Nguyễn Thị Nga</td>
+                            <td>Nguyễn Văn Bảo, Gò Vấp</td>
+                            <td>20:00 05/11/2024</td>
+                            <td>
+                                <button class="view-button" onclick="window.location.href='haucan_chuanbidonhang.php';">Xem chi tiết</button>
+                            </td>
+                        </tr>
+                        <!-- Add more rows as needed -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
