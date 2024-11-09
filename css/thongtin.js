4d78fa5104f61dd685546316a228e157c5f4dd17
huyentranlam <lamthihuyentran725@gmail.com>
2024-11-09 09:56:08 +0700

    Update thongtin.js

--- css/thongtin.js
+++ css/thongtin.js
@@ -1,15 +1,1 @@
 
-// JavaScript để ẩn/hiện sidebar ngay dưới biểu tượng
-function toggleSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("open");
-}
-
-// Đóng menu khi nhấp ra ngoài
-document.addEventListener("click", function(event) {
-    var sidebar = document.getElementById("sidebar");
-    var menuIcon = document.querySelector(".menu-icon");
-    if (!sidebar.contains(event.target) && !menuIcon.contains(event.target)) {
-        sidebar.classList.remove("open");
-    }
-});
