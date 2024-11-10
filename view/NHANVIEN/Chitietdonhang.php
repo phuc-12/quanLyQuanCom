c63712d7b0e42f03f0f4706557e8229b4fd1982f
huyentranlam <lamthihuyentran725@gmail.com>
2024-11-11 02:55:35 +0700

    Update Chitietdonhang.php

--- view/NHANVIEN/Chitietdonhang.php
+++ view/NHANVIEN/Chitietdonhang.php
@@ -4,24 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chi tiết đơn hàng</title>
-    <link rel="stylesheet" href="../../CSS/nhanVien.css">
+    <link rel="stylesheet" href="../../css/nhanVien.css">
+    <link rel="stylesheet" href="../../css/thongtin.css">
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../../js/dateTime.js" defer></script> 
+    <script src="../../js/thongtin.js" defer></script> 
 </head>
 <body>
     <!-- Header -->
+<div class="container-fluid p-0">
     <div class="header">
-        <div class="logo">Logo</div>
-        <h1><a href="Quanlidonhang.php">Quản lý đơn hàng</a></h1>
-        <div class="date">
-            <span>📅</span>
-            <span id="currentDate"></span>
-        </div>
-        <div class="user-icon">
-        <a href="ThongtinNhanVien.php">👤</a>
-        </div>
+        <div class="logo" style="float:left;">Logo</div>
+        <h1><a href="NV_quanli.php">Trang nhân viên</a></h1>
+        <div class="date" style="float:right; margin-right: 100px;"><span>📅</span><span id="currentDate"></span></div>
+        <div class="menu-icon" style="float:right;" onclick="toggleSidebar()">👤</div> 
     </div>
 
-    <div class="container">
+    <div class="sidebar" id="sidebar">
+    <button class="menu-item"><a href="ThongtinNhanVien.php">Thông tin cá nhân</a></button>        
+    <button class="menu-item"><a href="CN_thongtin.php">Cập nhật thông tin cá nhân</a></button>
+        <button class="menu-item"><a href="../../index.php">Đăng xuất</a></button>
+    </div>
+    <div class="container" style="width:100%;">
         <div class="content">
             <h2>Chi tiết đơn hàng</h2>
             <div class="inform">
@@ -65,6 +71,7 @@
             </div>
         </div>
     </div>
+</div>
 
 </body>
 </html>
