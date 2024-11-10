0ed3f3995492232d61ec0981ab4154b4638cf6f2
luongcongtrieu <luongcongtrieu210603@gmail.com>
2024-11-10 23:55:13 +0700

    updateNHANVIEN

--- view/NHANVIEN/Quanlidonhang.php
+++ view/NHANVIEN/Quanlidonhang.php
@@ -5,17 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý đơn hàng</title>
     <link rel="stylesheet" href="../../CSS/nhanVien.css">
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../../js/dateTime.js" defer></script> 
 </head>
 <body>
+    <div class="container-fluid p-0">
     <div class="header">
-        <div class="logo">Logo</div>
-        <h1><a href="NV_quanli.php">Trang nhân viên</a></h1>
-        <div class="date">
+        <div class="logo" style="float:left;">Logo</div>
+        <h1><a href="NV_quanli.php" >Trang nhân viên</a></h1>
+        <div class="date" style="float:right;">
             <span>📅</span>
             <span id="currentDate"></span>
         </div>
-        <div class="user-icon">
+        <div class="user-icon" style="float:right;">
             <a href="ThongtinNhanVien.php">👤</a>
         </div>
     </div>
@@ -51,6 +55,7 @@
                 </tbody>
             </table>
         </div>
+    </div>
     </div>
 
 </body>
