ee441610cd49a22fb54ab4e7bd49087e87d68cc1
huyentranlam <lamthihuyentran725@gmail.com>
2024-11-11 02:54:29 +0700

    Update Thongtin.php

--- view/KHACHHANG/Thongtin.php
+++ view/KHACHHANG/Thongtin.php
@@ -6,23 +6,27 @@
     <title> Thông tin khách hàng</title>
     <link rel="stylesheet" href="../../css/nhanVien.css">
     <link rel="stylesheet" href="../../css/thongtin.css">
-    <script src="../../js/dateTime.js" defer></script>
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../../js/dateTime.js" defer></script> 
     <script src="../../js/thongtin.js" defer></script>  
 </head>
 <body>
+<div class="container-fluid p-0">
     <div class="header">
-        <div class="logo">Logo</div>
-        <h1>Trang chủ</h1>
-        <div class="date"><span>📅</span><span id="currentDate"></span></div>
-        <div class="menu-icon" onclick="toggleSidebar()">👤</div> 
+        <div class="logo" style="float:left;">Logo</div>
+        <h1><a href="../../index.php">Trang chủ</a></h1>
+        <div class="date" style="float:right; margin-right: 100px;"><span>📅</span><span id="currentDate"></span></div>
+        <div class="menu-icon" style="float:right;" onclick="toggleSidebar()">👤</div> 
     </div>
         <div class="sidebar" id="sidebar">
-            <button class="menu-item">Thông tin cá nhân</button>
+        <button class="menu-item"><a href="Thongtin.php">Thông tin cá nhân</a></button>
             <button class="menu-item"><a href="CN_thongtin.php">Cập nhật thông tin cá nhân</a></button>
             <button class="menu-item">Theo dõi trạng thái đơn hàng</button>
             <button class="menu-item">Xem ưu đãi</button>
             <button class="menu-item">Xem lịch sử mua hàng </button>
-            <button class="menu-item">Đăng xuất</button>
+            <button class="menu-item"><a href="../../index.php">Đăng xuất</a></button>
         </div>
      <div class="container">
         <div class="content">
@@ -61,6 +65,7 @@
                 <input type="text" value="HCM" readonly>
             </div>
         </div>
+    </div>
     </div>
 </body>
 </html>
