073dfeee23ababc41b4abc39eaa75f7cd7ac0df2
ThPhuoc08 <ngophuoc949@gmail.com>
2024-11-11 01:54:52 +0700

    update

--- view/NHANVIEN/Thanhtoan.php
+++ view/NHANVIEN/Thanhtoan.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý đơn hàng</title>
     <link rel="stylesheet" href="../../CSS/nhanVien.css">
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../../js/dateTime.js" defer></script> 
 </head>
 <body>
+<div class="container-fluid p-0">
     <div class="header">
         <div class="logo">Logo</div>
         <h1><a href="NV_quanli.php">Trang nhân viên</a></h1>
@@ -75,6 +79,6 @@
             </table>
         </div>
     </div>
-
+</div>
 </body>
 </html>
--- view/NHANVIEN/Thongtinthanhtoan.php
+++ view/NHANVIEN/Thongtinthanhtoan.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thông tin thanh toán</title>
     <link rel="stylesheet" href="../../CSS/nhanVien.css">
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../../js/dateTime.js" defer></script> 
 </head>
 <body>
+<div class="container-fluid p-0">
     <!-- Header -->
     <div class="header">
         <div class="logo">Logo</div>
@@ -83,6 +87,6 @@
             </div>
         </div>
     </div>
-
+    </div>
 </body>
 </html>
