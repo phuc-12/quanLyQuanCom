a7504aebb756ad517838bd689524e713bd74bed5
huyentranlam <lamthihuyentran725@gmail.com>
2024-11-11 02:55:12 +0700

    Update CN_thongtin.php

--- view/NHANVIEN/CN_thongtin.php
+++ view/NHANVIEN/CN_thongtin.php
@@ -6,24 +6,28 @@
     <title>Cập nhật thông tin nhân viên</title>
     <link rel="stylesheet" href="../../css/nhanVien.css">
     <link rel="stylesheet" href="../../css/thongtin.css">
+    <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../../js/dateTime.js" defer></script> 
     <script src="../../js/thongtin.js" defer></script> 
 </head>
 <body>
+    <div class="container-fluid p-0">
     <div class="header">
-        <div class="logo">Logo</div>
+        <div class="logo" style="float:left;">Logo</div>
         <h1><a href="NV_quanli.php">Trang nhân viên</a></h1>
-        <div class="date"><span>📅</span><span id="currentDate"></span></div>
-        <div class="menu-icon" onclick="toggleSidebar()">👤</div> 
-    </div>
-    
-    <div class="sidebar" id="sidebar">
-        <button class="menu-item"><a href="ThongtinNhanVien.php">Thông tin cá nhân</a></button>
-        <button class="menu-item">Cập nhật thông tin cá nhân</button>
-        <button class="menu-item">Đăng xuất</button>
+        <div class="date" style="float:right; margin-right: 100px;"><span>📅</span><span id="currentDate"></span></div>
+        <div class="menu-icon" style="float:right;" onclick="toggleSidebar()">👤</div> 
     </div>
 
-    <div class="container">
+    <div class="sidebar" id="sidebar">
+    <button class="menu-item"><a href="ThongtinNhanVien.php">Thông tin cá nhân</a></button>
+        <button class="menu-item"><a href="CN_thongtin.php">Cập nhật thông tin cá nhân</a></button>
+        <button class="menu-item"><a href="../../index.php">Đăng xuất</a></button>
+    </div>
+
+    <div class="container" style="width:100%;">
         <div class="content">
             <h2>Cập nhật thông tin cá nhân</h2>
             <div class="form-container">
@@ -73,6 +77,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
 </body>
