b70ba2406a26fc538444fb1d448bb26061e947c0
tran <lamthihuyentran725@gmail.com>
2024-11-30 00:47:14 +0700

    css

--- view/NHANVIEN/Themdonmoi.php
+++ view/NHANVIEN/Themdonmoi.php
@@ -10,7 +10,7 @@ $sanphamList = $tmdt->layTatCaSanPham($maLoaiMA);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tạo đơn hàng</title>
-    <link rel="stylesheet" href="../../CSS/donhang.css">
+    <link rel="stylesheet" href="../../css/donhang.css">
     <link rel="stylesheet" href="../../css/thongtin.css">
     <link rel="stylesheet" href="../../css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
     <script src="../../css/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
