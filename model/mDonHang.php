c3d927ae698567c7a2ed79379c22e54f5f004058
tran <lamthihuyentran725@gmail.com>
2024-12-09 15:37:08 +0700

    //

--- model/mDonHang.php
+++ model/mDonHang.php
@@ -3,22 +3,22 @@
     include_once("chucnangnhanvien.php");
     include_once("ketnoi.php");
     class MHoaDon extends tmdt {
-        // public function SelectAllHD() {
-        //     $p = new tmdt();
-        //     $conn = $p->connect();
-        //     $conn->set_charset('utf8');
-        //     if($conn){
-        //         // $str = "select * from hoadon";
-        //         $str = "SELECT hd.maHD, hd.maKH,hd.ngayThang, kh.hoTen, hd.trangThaiGH
-        //             FROM hoadon hd
-        //             JOIN khachhang kh ON hd.maKH = kh.maKH";
-        //         $tblHD = $conn->query($str);
-        //         return $tblHD;
-        //     }else{
-        //         return false;
-        //     }
-        // }
-        // //
+        public function SelectAllHD() {
+            $p = new tmdt();
+            $conn = $p->connect();
+            $conn->set_charset('utf8');
+            if($conn){
+                // $str = "select * from hoadon";
+                $str = "SELECT hd.maHD, hd.maKH,hd.ngayThang, kh.hoTen, hd.trangThaiGH
+                    FROM hoadon hd
+                    JOIN khachhang kh ON hd.maKH = kh.maKH";
+                $tblHD = $conn->query($str);
+                return $tblHD;
+            }else{
+                return false;
+            }
+        }
+        //
         public function SelectAllDonHang() {
             $p = new clsKetNoi();
             $conn = $p->moketnoi();
